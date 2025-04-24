66eb6dc203385da75dc93ed9603fced041429c07
pupungeggang2 <kyongkyong2892@gmail.com>
2025-04-24 22:16:07 +0900

    Start

--- Program/Script/GLFunc.js
+++ Program/Script/GLFunc.js
@@ -24,4 +24,8 @@ class GL {
 
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0]), gl.STATIC_DRAW)
     }
+
+    static convertToBuffer(data) {
+        
+    }
 }
--- Program/Script/Primitive.js
+++ Program/Script/Primitive.js
@@ -5,3 +5,10 @@ class Vector3D {
         this.z = z
     }
 }
+
+class Cuboid3D {
+    constructor(x, y, z, w, h, d) {
+        this.position = new Vector3D(x, y, z)
+        this.size = new Vector3D(w, h, d)
+    }
+}
